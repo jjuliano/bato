5da5f337da78690d539a371c89561ccf1af35b10
Andrei Navarro <dreinavarro@gmail.com>
2016-06-29 15:21:10 +0800

    nagdagdag ng mga bagong pagsasalin

--- lib/bato/core_ext/array.rb
+++ lib/bato/core_ext/array.rb
@@ -5,7 +5,10 @@ class Array
   alias_method :isuksok, :inject
   alias_method :magbagsak, :drop
   alias_method :itulak, :push
+  alias_method :pasok, :push
   alias_method :huli, :last
+  alias_method :dulo, :last
   alias_method :una, :first
   alias_method :isaisahin, :each
+  alias_method :kada, :each
 end
