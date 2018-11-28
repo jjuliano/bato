af0d9011b1c61697edee194d56263227e3a5e2e5
Jason Bruno <jcwebhole@yahoo.com>
2018-11-28 19:03:45 +0800

    Additional Aliases for puts (#10)
      alias ilimbag puts
      alias mag_limbag puts
      alias itatak puts
      alias mag_tatak puts

--- lib/bato/core_ext/kernel.rb
+++ lib/bato/core_ext/kernel.rb
@@ -3,6 +3,10 @@ module Kernel
   alias sabihin puts
   alias iprint puts
   alias mag_print puts
+  alias ilimbag puts
+  alias mag_limbag puts
+  alias itatak puts
+  alias mag_tatak puts
   alias iangat raise
   alias itaas raise
   alias matulog sleep
